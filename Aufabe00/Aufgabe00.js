86fd4a2b531c2450d8c88869191527da889360ff
AnnaLotz <anna.lotz@web.de>
2019-03-21 10:48:48 +0100

    aufgabeNo.00

---
+++ Aufabe00/Aufgabe00.js
@@ -0,0 +1,13 @@
+var Aufg01;
+(function (Aufg01) {
+    window.addEventListener("DOMContentLoaded", init);
+    function init() {
+        let name = window.prompt("Wie heißt du?", "Name");
+        let begruesung = "Hallo " + name + "!";
+        //let container: HTMLElement = document.getElementById("firstDiv");
+        //container.innerText = ;
+        document.getElementById("firstDiv").innerText = begruesung;
+        console.log(begruesung);
+    }
+})(Aufg01 || (Aufg01 = {}));
+//# sourceMappingURL=Aufgabe00.js.map
