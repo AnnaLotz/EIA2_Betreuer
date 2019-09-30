98c6c99d579e6d5b2d29ec28a33fcc7c4be071bc
AnnaLotz <anna.lotz@web.de>
2019-09-30 15:17:16 +0200

    Try connecting to vscode

--- Aufabe00/Aufgabe00.ts
+++ Aufabe00/Aufgabe00.ts
@@ -2,7 +2,7 @@ namespace Aufg01 {
     window.addEventListener("DOMContentLoaded", init);
     function init(): void {
         let name: string = window.prompt("Wie heißt du?", "Name");
-        let begruesung: string = "Hallo " + name + "!";
+        let begruesung: string = "Hallo " + name + ", wilkommen bei EIA2!";
         //let container: HTMLElement = document.getElementById("firstDiv");
         //container.innerText = ;
         document.getElementById("firstDiv").innerText = begruesung;
